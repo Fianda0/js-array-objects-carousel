3bed6dc3f6a00bbfce604dda3c0a07f8d8585941
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-07 16:11:09 +0200

    Milestone 2

--- main.js
+++ main.js
@@ -25,5 +25,59 @@ const images = [
 
 //Creo un ciclo per aggiungere gli elementi in pagina
 images.forEach((element, i, array) => {
-    document.getElementById('principale').innerHTML += `<img src='${element.image}' </img>`
+    document.getElementById('principale').innerHTML += `
+    <div class="carta">
+    <img class='slide' src='${element.image}' </img>
+    <div class="testo"></div>
+ </div>`
 })
+
+
+let imgActive;
+let slideNext;
+
+// Creazione array con immagine delle slide
+let carta = document.getElementsByClassName('carta');
+let slideLateral = document.getElementsByClassName('slide-lateral');
+let addText = document.getElementsByClassName('testo');
+
+carta[0].classList.add('active')
+addText[0].innerHTML = `<h3>${images[0].title}</h3>${images[0].text}`
+
+document.getElementById('next').addEventListener('click', function () {
+
+
+    // Inizio ciclo for per controllare le immagini
+    for (let i = 0; i < carta.length; i++) {
+
+        const slideControl = carta[i]
+        const slideL = slideLateral[i]
+
+        // controllo slide se hanno la classe active
+        if (slideControl.classList.contains('active')) {
+            // se contine ACTIVE lo rimuovo
+            slideControl.classList.remove('active')
+            slideL.classList.remove('active', 'border-lateral')
+
+
+            // segno quale array ha la classe ACTIVE
+            imgActive = i;
+
+        }
+    }
+
+    // seleziono la prossima slide da attivare
+    slideNext = imgActive + 1;
+
+    // se mi trovo all'ulitma slide torno alla prima
+    if (slideNext == carta.length) {
+        slideNext = 0;
+    }
+
+    carta[slideNext].classList.add('active')
+    addText[slideNext].innerHTML = `<h3>${images[slideNext].title}</h3>${images[slideNext].text}`
+    slideLateral[slideNext].classList.add('active', 'border-lateral')
+
+
+})
+
